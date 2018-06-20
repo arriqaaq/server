6e63dd264bc0ca83aa199f2c893c3b5c97138737
Farhan <arriqaaq@yahoo.com>
2018-06-21 04:49:31 +0530

    implemented SO_REUSEPORT functionality partially

---
+++ example/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	glok "github.com/arriqaaq/server"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "Hello from %v!\n", os.Getpid())
+	})
+	server := glok.NewServer(mux, "0.0.0.0", 8080)
+	err := server.Serve()
+	log.Println("terminated", os.Getpid(), err)
+}
